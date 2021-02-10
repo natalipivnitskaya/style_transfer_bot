603fa6566961b3fd476531ef07af48efec06aaa4
natali <pivnitskaya.natali@gmail.com>
2021-02-10 14:22:47 -0800

    utils.py: keep size scale

--- utils.py
+++ utils.py
@@ -11,7 +11,8 @@ def load_image(filename, size=None, scale=None):
     
     img = Image.open(filename).convert('RGB')
     if size is not None:
-        img = img.resize((size, size), Image.ANTIALIAS)
+        size2 = int(size * 1.0 / img.size[0] * img.size[1])
+        img = img.resize((size, size2), Image.ANTIALIAS)
     elif scale is not None:
         img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
 
